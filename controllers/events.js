1aa62b93fa835fc9169b4912a5fa8ad2394ac79c
AndreaGul <andreagulino99@gmail.com>
2024-05-31 15:34:19 +0200

    feat:milestone 3 ricerca degli eventi tramite query

--- controllers/events.js
+++ controllers/events.js
@@ -2,32 +2,62 @@ const { events: modelEvents } = require('../models/events');
 const allEvents= modelEvents.getAllEvents();
 
 const index = ( req,res )=>{
+    const { id } = req.query;
 
-    res.format({
-        html:()=>{
-            let html = '<main><ul>';
-            allEvents.forEach(({id,title,description,date,maxSeats})=>{
-                html+=`
-                <li>${id}</li>
-                <li>${title}</li>
-                <li>${description}</li>
-                <li>${date}</li>
-                <li>${maxSeats}</li>`;
-                
+    if(id){
+        const idEvento = parseInt(id);
+        const eventoRichiesto = allEvents.find( film => film.id === idEvento);
+        if(eventoRichiesto){
+            res.format({
+                html: ()=>{
+                    const { id, title, description, date, maxSeats} = eventoRichiesto
+                let html = '<main><ul>';
+                    html += `
+                    <li>id: ${id}</li>
+                    <li>titolo: ${title}</li>
+                    <li>descrizione: ${description}</li>
+                    <li>data: ${date}</li>
+                    <li>Numero massimo di posti: ${maxSeats}</li>
+                    `;
+                html += '</ul></main>'; 
+                res.send(html); 
+            },
+            json:()=>{
+                if(eventoRichiesto){
+                    res.json({
+                        ...eventoRichiesto,
+                    })
+                }
+            }
             });
-            html += '</ul><main>';
-            res.send(hmtl);
-        },
-        json:()=>{
-            res.json(allEvents);
+        }else
+        {
+            res.status(404).send('Event non trovato');
         }
         
-    });
-};
-
-const show = ( req,res )=>{
-     res.send('<h1>Sei nell\'index</h1>')};
+    } else{
+        res.format({
+            html: () => {
+                let html = '<main><ul>';
+                allEvents.forEach(({ id, title, description, date, maxSeats }) => {
+                    html += `
+                    <li>id: ${id}</li>
+                    <li>titolo: ${title}</li>
+                    <li>descrizione: ${description}</li>
+                    <li>data: ${date}</li>
+                    <li>Numero massimo di posti: ${maxSeats}</li>
+                    `;
+                });
+                html += '</ul></main>'; 
+                res.send(html); 
+            },
+            json: () => {
+                res.json(allEvents);
+            }
+        });
+    }
     
+};
 
 const store = ( req,res )=>{};
 
